48a6b3449296a45e0b7ea8374aa8f2bc0d259684
tanvi alok <talok535@gmail.com>
2023-06-23 14:59:16 -0400

    Initial Commit

---
+++ abtMe/abtMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  abtMeApp.swift
+//  abtMe
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct abtMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
